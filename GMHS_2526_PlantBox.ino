80f04ac3f7cac8e32c86b0e90511384c92163831
jordan-deitsch <jordan.b.deitsch@gmail.com>
2025-12-06 19:58:21 -0700

    Update header and readme

--- GMHS_2526_PlantBox.ino
+++ GMHS_2526_PlantBox.ino
@@ -1,27 +1,3 @@
-/******************************************************************************
-GMHS_2526_PlantBox.ino
-Arduino controller for plant box sensors and motors
-
-Modified from:
-TB6612FNG H-Bridge Motor Driver Example code
-Michelle @ SparkFun Electronics
-8/20/16
-https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library
-
-Uses 1 motor and 1 moisture sensor to add moisture to a plant grow-bed.
-Activate the motor for a finite time when the moisture drops below a 
-minimum threshold to extend lead screw. Retract the motor lead screw 
-when fully extended to allow for removal/replacement of water bag.
-
-Use I2C accelerometer to detect changes in gravity and when plant box 
-is in null-gravity environment.  
-
-Resources:
-TB6612 SparkFun Library
-BMA400 SparkFun Library
-******************************************************************************/
-
-// This is the library for the TB6612 that contains the Motor class and functions
 #include "src/MotorDriver.h"
 #include "src/AccelerometerDriver.h"
 #include "src/TimedFunction.h"
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# GMHS_2526_PlantBox
-Arduino code for motor controller and sensor peripherals of the low-gravity plant grow box. 
+GMHS_2526_PlantBox.ino
+Arduino controller for plant box sensors and motors
+
+Functionality:
+1) Use 1 motor and 1 moisture sensor to add moisture to a plant grow-bed.
+Activate the motor for a finite time when the moisture drops below a 
+minimum threshold to extend lead screw. Retract the motor lead screw 
+when fully extended to allow for removal/replacement of water bag.
+
+2) Use I2C accelerometer to detect changes in gravity and when plant box 
+is in null-gravity environment.  
+
+Resources:
+TB6612 SparkFun Library: https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library
+BMA400 SparkFun Library: https://github.com/sparkfun/SparkFun_BMA400_Arduino_Library
